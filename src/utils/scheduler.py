ae8a565e85a8278b5739e7b65ea76cabbfe31261
jjjohny228 <gggleb13@gmail.com>
2025-12-01 17:16:10 +0200

    Changed scheduler interval time to 5 minutes

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 def schedule_func(func):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(func=func, trigger="interval", seconds=20)
+    scheduler.add_job(func=func, trigger="interval", minutes=5)
     scheduler.start()
